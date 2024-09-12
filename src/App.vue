7faaed26b1802b8ec2b356ba026bfac9857ec49d
SammyHuang12 <jessie_hkimo@yahoo.com.tw>
2024-09-12 15:03:32 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<!-- 寫 JS 區塊 -->
+<script setup>
+// 可以import vue 元件
+// import HelloWorld from './components/HellWorld.vue'
+
+  import {ref} from 'vue';
+  
+  const num = ref(2);
+  function clickMe(){
+    num.value++
+  }
+</script>
+
+<!-- html 的片段 -->
+<template>
+  <div>
+    {{ num }}
+    <button type="button" v-on:click="clickMe">點我+1</button>
+  </div>
+</template>
+
+
+<!-- css 片段 -->
+ <style csoped>
+</style>
---
+++ src/main.js
@@ -0,0 +1,7 @@
+import './assets/main.css'      //主要 CSS 滙入
+
+import { createApp } from 'vue'
+import App from './App.vue'     //App 元件
+
+
+createApp(App).mount('#app')
